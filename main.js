15382021360e06c29cbb2b9a90e013e7bb8626fe
borjarobert <borja@brobert.net>
2020-11-17 14:25:04 +0100

    cambia urls

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function showManifiesto() {
   manifiesto.classList.remove("inv");
   firmantes.classList.add("inv");
   firma.classList.add("inv");
+  window.history.pushState("", "Manifiesto de juristas por la eutanasia", "");
 }
 
 function showFirmantes() {
@@ -28,6 +29,7 @@ function showFirma() {
   manifiesto.classList.add("inv");
   firma.classList.remove("inv");
   firmantes.classList.add("inv");
+  window.history.pushState("", "Firma el manifiesto de juristas por la eutanasia", "firma");
 }
 
 manifiestoLink.addEventListener("click", showManifiesto, false);
